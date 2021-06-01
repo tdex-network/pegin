4edd3edf03fa16aa30a1c2fec8b5ae36dec1314a
Louis Singer <41042567+louisinger@users.noreply.github.com>
2021-06-01 13:20:30 +0200

    Fix MainChainAddress generation + add claim function to ElementsPegin (#4)
    * npm i
    
    * add --recursive to README submoduble init
    
    * update go-elements pegin package and enrich getMainchainAddress tests
    
    * claimTx in ElementsPegin
    
    * remove unused deps
    
    * do not persist crendential in gh CI
    
    * npm install in gh ci
    
    * revert main.yml
    
    * --recursive in Compile WASM step
    
    * add reconfigure step in GH CI

--- resources/go/go.mod
+++ resources/go/go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/btcsuite/btcd v0.21.0-beta
-	github.com/vulpemventures/go-elements v0.2.1-0.20210506082611-fc6154adfabf
-	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
+	github.com/vulpemventures/go-elements v0.3.0
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 )
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 interface ElementsPeginInterface {
+  claimTx(
+    btcTx: string,
+    btcTxOutProof: string,
+    claimScript: string,
+    millisatPerByte: number
+  ): Promise<string>; // returns the claim tx (hex encoded)
   getMainchainAddress(claimScript: string): Promise<string>; // returns the mainchain address
 }
 
@@ -6,12 +12,27 @@ type PeginModuleOption = (module: ElementsPegin) => void;
 
 export default class ElementsPegin implements ElementsPeginInterface {
   private wallycore: any;
-  private peginAddress:
+  private mainChainAddress:
     | ((
         contract: string,
         fedPegScript: string,
         isDynaFed: boolean,
         isMainnet: boolean
+      ) => Promise<string>)
+    | undefined;
+
+  private claim:
+    | ((
+        isMainnet: boolean,
+        isDynaFed: boolean,
+        peggedAsset: string,
+        parentGenesisBlockHash: string,
+        fedPegScript: string,
+        contract: string,
+        btcTx: string,
+        btcTxOutProof: string,
+        claimScript: string,
+        millisatPerByte: number
       ) => Promise<string>)
     | undefined;
 
@@ -25,10 +46,42 @@ export default class ElementsPegin implements ElementsPeginInterface {
     }
   }
 
-  async getMainchainAddress(claimScript: string): Promise<string> {
-    if (!this.peginAddress)
+  async claimTx(
+    btcTx: string,
+    btcTxOutProof: string,
+    claimScript: string,
+    millisatPerByte: number = 0.1
+  ): Promise<string> {
+    if (!this.claim) throw new Error('need claim to be defined');
+
+    if (!this.wallycore)
       throw new Error(
-        'need peginAddress to be defined in order to compute mainchain address'
+        'need wallycore to be defined in order to compute mainchain address'
+      );
+
+    const contract = await this.peginContract(
+      this.federationScript,
+      claimScript
+    );
+
+    return this.claim(
+      this.isMainnet,
+      this.isDynamicFederation,
+      this.getPeggedAsset(),
+      ElementsPegin.PARENT_BLOCK_HASH,
+      this.federationScript,
+      contract,
+      btcTx,
+      btcTxOutProof,
+      claimScript,
+      millisatPerByte
+    );
+  }
+
+  async getMainchainAddress(claimScript: string): Promise<string> {
+    if (!this.mainChainAddress)
+      throw new Error(
+        'need mainChainAddress to be defined in order to compute mainchain address'
       );
 
     if (!this.wallycore)
@@ -40,13 +93,14 @@ export default class ElementsPegin implements ElementsPeginInterface {
       this.federationScript,
       claimScript
     );
-    const peginAddress = await this.peginAddress(
+
+    const address = await this.mainChainAddress(
       contract,
       this.federationScript,
       this.isDynamicFederation,
       this.isMainnet
     );
-    return peginAddress;
+    return address;
   }
 
   /**
@@ -108,35 +162,43 @@ export default class ElementsPegin implements ElementsPeginInterface {
     return bytes;
   }
 
+  private getPeggedAsset(): string {
+    return this.isMainnet
+      ? '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d'
+      : '5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225';
+  }
+
   public static withFederationScript(federationScript: string) {
-    return (module: ElementsPegin) => {
-      module.federationScript = federationScript;
+    return (mod: ElementsPegin) => {
+      mod.federationScript = federationScript;
     };
   }
 
   public static withDynamicFederation(dynaFed: boolean) {
-    return (module: ElementsPegin) => {
-      module.isDynamicFederation = dynaFed;
+    return (mod: ElementsPegin) => {
+      mod.isDynamicFederation = dynaFed;
     };
   }
 
   public static withMainnet() {
-    return (module: ElementsPegin) => {
-      module.isMainnet = true;
+    return (mod: ElementsPegin) => {
+      mod.isMainnet = true;
     };
   }
 
   public static withTestnet() {
-    return (module: ElementsPegin) => {
-      module.isMainnet = false;
+    return (mod: ElementsPegin) => {
+      mod.isMainnet = false;
     };
   }
 
   public static async withGoElements(): Promise<PeginModuleOption> {
-    await runGoWASMinstance(); // set peginAddress in JS global scope
+    await runGoWASMinstance(); // set mainChainAddress in JS global scope
     return (p: ElementsPegin): void => {
       // @ts-ignore
-      p.peginAddress = peginAddress;
+      p.mainChainAddress = mainChainAddress;
+      // @ts-ignore
+      p.claim = claim;
     };
   }
 
@@ -148,6 +210,9 @@ export default class ElementsPegin implements ElementsPeginInterface {
       p.wallycore = wallycore;
     };
   }
+
+  public static PARENT_BLOCK_HASH =
+    '0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206';
 
   public static MAINNET_FED_SCRIPT =
     '745c87635b21020e0338c96a8870479f2396c373cc7696ba124e8635d41b0ea581112b678172612102675333a4e4b8fb51d9d4e22fa5a8eaced3fdac8a8cbf9be8c030f75712e6af992102896807d54bc55c24981f24a453c60ad3e8993d693732288068a23df3d9f50d4821029e51a5ef5db3137051de8323b001749932f2ff0d34c82e96a2c2461de96ae56c2102a4e1a9638d46923272c266631d94d36bdb03a64ee0e14c7518e49d2f29bc40102102f8a00b269f8c5e59c67d36db3cdc11b11b21f64b4bffb2815e9100d9aa8daf072103079e252e85abffd3c401a69b087e590a9b86f33f574f08129ccbd3521ecf516b2103111cf405b627e22135b3b3733a4a34aa5723fb0f58379a16d32861bf576b0ec2210318f331b3e5d38156da6633b31929c5b220349859cc9ca3d33fb4e68aa08401742103230dae6b4ac93480aeab26d000841298e3b8f6157028e47b0897c1e025165de121035abff4281ff00660f99ab27bb53e6b33689c2cd8dcd364bc3c90ca5aea0d71a62103bd45cddfacf2083b14310ae4a84e25de61e451637346325222747b157446614c2103cc297026b06c71cbfa52089149157b5ff23de027ac5ab781800a578192d175462103d3bde5d63bdb3a6379b461be64dad45eabff42f758543a9645afd42f6d4248282103ed1e8d5109c9ed66f7941bc53cc71137baa76d50d274bda8d5e8ffbd6e61fe9a5f6702c00fb275522103aab896d53a8e7d6433137bbba940f9c521e085dd07e60994579b64a6d992cf79210291b7d0b1b692f8f524516ed950872e5da10fb1b808b5a526dedc6fed1cf29807210386aa9372fbab374593466bc5451dc59954e90787f08060964d95c87ef34ca5bb5368ae';
